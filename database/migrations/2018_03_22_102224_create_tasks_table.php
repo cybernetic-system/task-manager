d3548d8d032c002227d796de40bd35d88a2c2b46
Vladimir <bukvinov.mmmedia@gmail.com>
2018-03-23 17:58:59 +0300

    add fields into tasks migration

--- database/migrations/2018_03_22_102224_create_tasks_table.php
+++ database/migrations/2018_03_22_102224_create_tasks_table.php
@@ -17,7 +17,14 @@ class CreateTasksTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description');
-            $table->integer('status');
+            $table->timestamp('time_laid')->nullable();//времени заложено
+            $table->timestamp('time_spend')->nullable();//времени затрачено
+            $table->timestamp('last_time_start')->nullable();//последний запуск таймера
+            $table->boolean('start_status')->default(0);//статус запуска
+            $table->integer('status_id')->unsigned();
+            $table->integer('priority_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('group_id')->unsigned();
             $table->timestamps();
         });
     }
